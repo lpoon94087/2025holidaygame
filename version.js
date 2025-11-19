7ece0937be41aacf48345bbd97fc0dfe5634b2dc
lpoon <lucidpixels.instantid@gmail.com>
2025-11-19 13:21:39 -0800

    use different keys for mini

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-const GIT_VERSION = 'v1.0.9';
+const GIT_VERSION = 'v1.0.10';
 const VERSION_PREFIX = '1.0';
-const SEQUENCE = 9;
-const GIT_DATE = '2025-11-18';
+const SEQUENCE = 10;
+const GIT_DATE = '2025-11-19';
