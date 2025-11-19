4628bfab48e6f316899d9f262319bfbaaa92b433
lpoon <lucidpixels.instantid@gmail.com>
2025-11-18 17:02:12 -0800

    Add bonus

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-const GIT_VERSION = 'v1.0.4';
+const GIT_VERSION = 'v1.0.5';
 const VERSION_PREFIX = '1.0';
-const SEQUENCE = 4;
-const GIT_DATE = '2025-11-15';
+const SEQUENCE = 5;
+const GIT_DATE = '2025-11-18';
