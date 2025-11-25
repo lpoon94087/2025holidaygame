fff8436cf553e54b7427262776b154a82a976c72
lpoon <lucidpixels.instantid@gmail.com>
2025-11-24 20:27:56 -0800

    fixed post score

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-const GIT_VERSION = 'v1.0.11';
+const GIT_VERSION = 'v1.0.12';
 const VERSION_PREFIX = '1.0';
-const SEQUENCE = 11;
-const GIT_DATE = '2025-11-19';
+const SEQUENCE = 12;
+const GIT_DATE = '2025-11-24';
