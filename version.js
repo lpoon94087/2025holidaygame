de27c8257146b845c2bc189f983ef3e59f8de5fd
lpoon <lucidpixels.instantid@gmail.com>
2025-12-01 12:09:28 -0800

    Add improved guesstheperson

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-const GIT_VERSION = 'v1.0.14';
+const GIT_VERSION = 'v1.0.15';
 const VERSION_PREFIX = '1.0';
-const SEQUENCE = 14;
-const GIT_DATE = '2025-11-29';
+const SEQUENCE = 15;
+const GIT_DATE = '2025-12-01';
